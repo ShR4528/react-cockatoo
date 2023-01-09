d94a6231076454f98a83556b407518a956fc7557
Shr4528 <Shr4528@gmail.com>
2023-01-09 14:12:58 +0200

    id

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function TodoList() {
-    const todoList = ['item 1', 'item 2', 'item 3'];
+    const todoList = [
+        { id: 1, title: 'item 1' },
+        { id: 2, title: 'item 2' },
+        { id: 3, title: 'item 3' },
+    ];
 
     return (
         <ul>
